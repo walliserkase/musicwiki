8fe8a2f76369fa0535194bf47670873aa323c869
Elie <elie.kin63@gmail.com>
2017-01-22 16:28:36 +0100

    MAJ

---
+++ Insertion_artiste.sql
@@ -0,0 +1,2 @@
+insert into interprete (nomArtiste, nom, prenom, dateNaissance) values ("Johnny Hallyday", "Johnny", "Hallyday", "1945-06-10");
+insert into interprete (nomArtiste, nom, prenom, dateNaissance) values ("Michael Jackson", "Michael", "Jackson", "1958-08-29");
